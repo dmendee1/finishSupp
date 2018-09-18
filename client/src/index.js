b174c18549e87b12a34deb118de46e3b2e23829c
dmendee1 <mendeemendee9@gmail.com>
2018-09-18 11:26:18 +0800

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch, hashHistory } from "react-router-dom";
 import registerServiceWorker from './registerServiceWorker';
 
 import "./assets/css/material-dashboard-react.css";
@@ -11,10 +10,8 @@ import store from './store';
 import indexRoutes from "./routes/index.jsx";
 require('../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css');
 
-const hist = createBrowserHistory();
-
 ReactDOM.render(
-  <Router history={hist}>
+  <Router history={hashHistory}>
     <Switch>
       {indexRoutes.map((prop, key) => {
         return (
